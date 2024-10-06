0a3ff56fe04f10a861df32cc547b27fefefc1d73
Niccolò Calabrese <nicchecala@gmail.com>
2024-10-06 10:47:25 +0200

    Create folder anecdotes and Done exercise 1.12

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // import App from './courseinfo/App.jsx'
-import App from './unicafe/App.jsx'
-// import App from './anecdotes/App.jsx'
+// import App from './unicafe/App.jsx'
+import App from './anecdotes/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
