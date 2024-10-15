67704504df5df7f26e90cc3496baa8c557d36e87
Niccolò Calabrese <nicchecala@gmail.com>
2024-10-15 17:41:14 +0200

    done practices useEffect() with Note-app

--- part2/src/noteProjectPractice/App.jsx
+++ part2/src/noteProjectPractice/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useEffect, useState } from 'react'
+import Note from './components/Note.jsx'
+import axios from "axios";
 
-const App = (props) => {
-  const [ notes, setNotes ] = useState(props.notes)
-  const [ newNote, setNewNote ] = useState(
-    'a new note...'
-  )
-  const [ showAll, setShowAll ] = useState(false)
+const App = () => {
+  const [ notes, setNotes ] = useState([])
+  const [ newNote, setNewNote ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
