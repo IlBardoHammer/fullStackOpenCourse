87dc1df7bf136bbecf25703a85eafa6e55f81ba9
ilbardohammer <nicchecala@gmail.com>
2024-11-01 17:34:01 +0100

    new line endings macOS

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './ThePhonebook/App'
-import "./ThePhonebook/index.css"
+import App from './dataCountriesApp/App'
+import "./noteProjectPractice/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App/>
