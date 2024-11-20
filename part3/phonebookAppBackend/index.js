830d05b99610bb4b6171c0790ac070f792ea54ee
ilbardohammer <nicchecala@gmail.com>
2024-11-20 16:06:59 +0100

    uibuild

--- part3/phonebookAppBackend/index.js
+++ part3/phonebookAppBackend/index.js
@@ -36,12 +36,12 @@ const generateId = () => {
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', function (req) {
   return JSON.stringify(req.body)
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // ROUTE HANDLER
 
