61feed11fc826242f67145e204b09a1a0cb6a858
ilbardohammer <nicchecala@gmail.com>
2024-11-29 17:03:23 +0100

    fix catch

--- part3/noteAppBackend/models/note.js
+++ part3/noteAppBackend/models/note.js
@@ -12,7 +12,7 @@ mongoose.connect(url)
     console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+    console.log('error connecting to MongoDB:', error)
   })
 
 const noteSchema = new mongoose.Schema({
